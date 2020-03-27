abd79c8b44526035cafae1f69c50441123b198cb
Izzatunnisa Arwadi <nisarwd@gmail.com>
2020-03-27 20:09:38 +0700

    Tutorial 07 v1

---
+++ tutorial-07/src/main/java/com/apap/tu07/service/FlightService.java
@@ -0,0 +1,21 @@
+package com.apap.tu07.service;
+
+import java.util.List;
+
+import com.apap.tu07.model.FlightModel;
+
+/**
+ * FlightService
+ */
+public interface FlightService {
+    
+    FlightModel addFlight(FlightModel flight);
+	
+    void updateFlight(FlightModel flightModel);
+	
+    FlightModel flightById(long id);
+	
+    void removeFlight(long id);
+	
+    List <FlightModel> getAllFlight();
+}
